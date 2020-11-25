deb08ab24f1f480a8148a91c12ef9c9a85420858
Fedor Gorbunov <raigfp@gmail.com>
2020-11-25 06:42:08 +0000

    DIM csv import

--- transfer-tags.py
+++ transfer-tags.py
@@ -1,12 +1,9 @@
 import json
-import os
+import csv
+
 
 CHECKLIST_FILENAME = 'd2checklist-tags.json'
-DIM_FILENAME = 'dim-data.json'
-DIM_RESULT_FILENAME = 'dim-data-result.json'
-
-DIM_PLATFORM_MEMBERSHIP_ID = '4611686018461090495'
-DIM_DESTINY_VERSION = 2
+DIM_FILENAME = 'dim.csv'
 
 TAG_MAPPING = {
     'upgrade': 'favorite',
@@ -15,30 +12,20 @@ TAG_MAPPING = {
     'junk': 'junk',
 }
 
+
 if __name__ == '__main__':
-    try:
-        dim_file = open(DIM_FILENAME, 'r')
-        dim_data = json.loads(dim_file.read())
-
-        checklist_file = open(CHECKLIST_FILENAME, 'r')
+    with open(CHECKLIST_FILENAME, 'r') as checklist_file:
         checklist_data = json.loads(checklist_file.read())
-    except FileNotFoundError:
-        exit(f'Put {CHECKLIST_FILENAME} and {DIM_FILENAME} in the directory.')
+        dim_rows = [('Id', 'Hash', 'Tag', 'Notes')] + [
+            (
+                id,
+                '',
+                TAG_MAPPING[tag],
+                '',
+            )
+            for id, tag in checklist_data['marked'].items()
+        ]
 
-    dim_data['tags'] = [
-        {
-            'platformMembershipId': DIM_PLATFORM_MEMBERSHIP_ID,
-            'destinyVersion': DIM_DESTINY_VERSION,
-            'annotation': {
-                'id': id,
-                'tag': TAG_MAPPING[tag],
-            },
-        }
-        for id, tag in checklist_data['marked'].items()
-    ]
-
-    dim_result_file = open(DIM_RESULT_FILENAME, 'w')
-    dim_result_file.write(json.dumps(dim_data))
-
-    os.remove(CHECKLIST_FILENAME)
-    os.remove(DIM_FILENAME)
+    with open(DIM_FILENAME, 'w') as dim_file:
+        dim_writer = csv.writer(dim_file, delimiter=',')
+        dim_writer.writerows(dim_rows)
